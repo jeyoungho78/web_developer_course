f5078c928bd9fd547c3ccc2107f1c4cabdd23024
INC <INC@A14>
2017-11-28 14:18:24 +0900

    first commit

---
+++ rank/Data.java
@@ -0,0 +1,55 @@
+package data.rank;
+
+public class Data {
+	String name;
+	int kor;
+	int eng;
+	int math;
+	int physic;
+	int totalsum;
+	double totalavg;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getKor() {
+		return kor;
+	}
+	public void setKor(int kor) {
+		this.kor = kor;
+	}
+	public int getEng() {
+		return eng;
+	}
+	public void setEng(int eng) {
+		this.eng = eng;
+	}
+	public int getMath() {
+		return math;
+	}
+	public void setMath(int math) {
+		this.math = math;
+	}
+	public int getPhysic() {
+		return physic;
+	}
+	public void setPhysic(int physic) {
+		this.physic = physic;
+	}
+	public int getTotalsum() {
+		return totalsum;
+	}
+	public void setTotalsum(int totalsum) {
+		this.totalsum = totalsum;
+	}
+	public double getTotalavg() {
+		return totalavg;
+	}
+	public void setTotalavg(double totalavg) {
+		this.totalavg = totalavg;
+	}
+	
+}
